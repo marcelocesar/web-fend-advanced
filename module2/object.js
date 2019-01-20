1c673b06d536d3119bf5ef2b86ad9ea4ad2b01ff
Marcelo Cesar <marcelop3@gmail.com>
2019-01-19 22:38:27 -0200

    Objects in JS

---
+++ module2/object.js
@@ -0,0 +1,194 @@
+/**
+ * 1. Objetos
+ */
+
+// Objetos é um conjunto de pares chave/valor associados. Criamos um objeto com chaves (ou seja, { e }).
+
+const myObject = {};
+
+// Propriedades com pares de chave:valor.
+const car = {
+    color: 'red',
+    year: 1992,
+    isPreOwned: true
+};
+
+//Objetos são desordenados.
+const anotheCar = {
+    isPreOwned: true,
+    color: 'red',
+    year: 1992
+};
+
+// Lendo objetos.
+const greetings = {
+    hello: [{
+        english: 'hi',
+        french: 'bonjour',
+        portuguese: 'oi'
+    }],
+    goodbye: [{
+        english: 'bye',
+        french: 'au revoir',
+        portuguese: 'tchau'
+    }]
+};
+
+console.log(greetings.hello[0].portuguese);
+console.log(greetings['hello'][0]['portuguese']);
+
+// Resumo
+// Em JavaScript, um objeto é um conjunto não ordenado de propriedades. 
+// Cada propriedade é composta de um par chave/valor e pode referenciar um primitivo 
+// (como strings, números, booleanos etc.) ou outro objeto. Diferentemente dos elementos de uma matriz, 
+// que são acessados por um índice numérico, as propriedades dos objetos são acessadas pelo nome de chave, 
+// usando notação de colchetes ou notação de ponto. 
+
+
+/**
+ * 2. Criando objetos
+ */
+
+// usando notação literal:
+const pessoal = {};
+
+// usando a função construtora Object():
+const animal = new Object();
+
+//Recomendado utilizar a notação literal
+
+
+/** 
+ * Modificando propriedades
+ */
+
+const dog = {
+    name: "Marley",
+    age: 2,
+    bark: () => console.log('Auauau!'),
+    greet: (name) => console.log(`${name} auuuuu...!`)
+}
+
+dog.age += 1;
+console.log(dog.age);
+
+dog.name = "Bob"
+console.log(dog.name);
+console.log(dog.greet('Fox'));
+
+
+// Adicionando propriedades
+
+const orc = {};
+orc.strength = 40;
+orc.dexterity = 28;
+console.log(orc);
+
+orc['magic'] = 15;
+console.log(orc);
+
+orc.attack = () => {
+    console.log(`Damage is 68!!!!`);
+}
+
+console.log(orc.attack());
+
+
+// Removendo propriedades
+
+delete orc.magic;
+
+console.log(orc);
+
+
+/**
+ * Passando argumentos
+ */
+
+// Argumento primitivo int
+
+function changeToEight(n) {
+    n = 8;
+}
+
+let n = 7;
+changeToEight(n);
+console.log(n);
+
+// Argumento objeto
+
+let obj = {
+    color: 'red'
+};
+
+function setToBlue(object) {
+    object.color = 'blue';
+}
+
+setToBlue(obj);
+console.log(obj.color); //blue
+
+// Como isso aconteceu? Bom, como os objetos no JavaScript são passados por referência, 
+// se fizermos mudanças nessa referência, estaremos modificando o objeto original diretamente!
+
+// Além disso, a mesma regra se aplica quando reatribuímos um objeto a uma nova variável e mudamos 
+// essa cópia. Repetindo: como os objetos são passados por referência, o objeto original também é alterado.
+
+const developers = {
+    marcelo: 3,
+    cesar: 5
+};
+
+const developersCopy = developers;
+
+console.log(developersCopy.cesar); // 5
+
+developersCopy.cesar = 333;
+
+console.log(developersCopy.cesar); // 333
+
+console.log(developers.cesar); // 333
+
+//Como os objetos são passados por referência, fazer mudanças à cópia 
+//(developersCopy) produz efeito direto no objeto original.
+
+/**
+ * Comparando objetos
+ */
+
+const marcelo = {
+    name: 'marcelo',
+    age: 35,
+    job: () => {
+        console.log('Front-End Developer!');
+    }
+};
+
+const cesar = {
+    name: 'marcelo',
+    age: 35,
+    job: () => {
+        console.log('Front-End Developer!');
+    }
+};
+
+console.log(marcelo === cesar); //false - objetos iguais, porém, referência diferentes
+
+const jesus = marcelo;
+
+console.log(marcelo === jesus); //true - objetos iguais e referência iguais.
+
+
+/*
+Resumo
+Objetos são comumente criados com notação literal e podem conter propriedades que apontam para 
+funções chamadas de métodos. Os métodos são acessados da mesma forma que outras propriedades de 
+objetos e podem ser invocados da mesma forma que as funções, a diferença é que eles têm acesso 
+automaticamente às outras propriedades do objeto-pai.
+
+Por padrão, objetos são mutáveis (com algumas exceções), então os dados dentro deles podem ser alterados. 
+Novas propriedades podem ser adicionadas, e as propriedades que já existem podem ser modificadas 
+simplesmente especificando o nome da propriedade e atribuindo (ou reatribuindo) um valor. Além disso, 
+propriedades e métodos de um objeto podem ser excluídos também com o operador delete, que modifica o objeto diretamente.
+*/
+
